9a6466dc6c168e0fca796d147fcb7685e5c457df
Olayioye  Adedayo <deyupiano@yahoo.com>
2023-10-04 15:10:50 +0100

    add store commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,30 @@
-import { Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
-export function Navbar(){
- return (
+export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart()
+  return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
-        <Container>
-            <Nav className="me-auto">
-                <Nav.Link to="/" as = {NavLink}>Home</Nav.Link>
-                <Nav.Link to="/store" as = {NavLink}>Store</Nav.Link>
-                <Nav.Link to="/about" as = {NavLink}>About</Nav.Link>
-            </Nav>
-            <Button
-                style={{ width: "3rem", height: "3rem", position: "relative" }}
-                variant="outline-primary"
-                className="rounded-circle"
-            >
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 576 512"
@@ -35,10 +45,11 @@ export function Navbar(){
                 transform: "translate(25%, 25%)",
               }}
             >
-               3 
+              {cartQuantity}
             </div>
-            </Button>
-        </Container>
+          </Button>
+        )}
+      </Container>
     </NavbarBs>
- )
+  )
 }
